7e483296f895028f1f0a4b2c1040b716cafd7118
jhosuapp <jhosuapvll@gmail.com>
2025-12-01 11:41:38 -0500

    feat: conditionals

---
+++ 02-python-basics/03-conditionals.py
@@ -0,0 +1,11 @@
+
+is_new_dev = False
+knows_python = False
+
+if is_new_dev:
+    print("Te recomiendo saber Python.")
+else:
+    print("Inicia en este mundo de la programación.")
+    
+if knows_python:
+    print("Te recomiendo aprender Django.")
