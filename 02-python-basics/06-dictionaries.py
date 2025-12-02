4b08ce59322b09cb2b1cde3931c3dc3c2e375e19
jhosuapp <jhosuapvll@gmail.com>
2025-12-01 21:41:47 -0500

    feat: dictionaries

---
+++ 02-python-basics/06-dictionaries.py
@@ -0,0 +1,12 @@
+user = {
+    'name': 'Jhosua',
+    'email': 'jhosuapvll@gmail.com',
+    'age': 24
+}
+
+user['age'] = 25
+user['country'] = 'Colombia'
+print(user)
+
+# values, items, keys
+print(user.items())
