6c831ec6d0166741d8851dc4e251982ac42ee7fe
jhosuapp <jhosuapvll@gmail.com>
2025-12-08 17:43:50 -0500

    feat: args && kwargs

--- 02-python-basics/11-functions.py
+++ 02-python-basics/11-functions.py
@@ -1,7 +1,14 @@
-# Parámetros
-def hello(greet, name):
-    print(f"{greet}, {name}")
+# # Parámetros
+# def hello(greet, name):
+#     print(f"{greet}, {name}")
     
-# Argumentos
-hello('Hola', 'Jhosua')
-hello(name='Jhos', greet='Hola')
+# # Argumentos
+# hello('Hola', 'Jhosua')
+# hello(name='Jhos', greet='Hola')
+
+def big_function(*args, **kwargs):
+    print(args)
+    print(kwargs)
+    return kwargs
+
+print(big_function(1,2,3,4,5,6,7,8, num=100, num2=200))
