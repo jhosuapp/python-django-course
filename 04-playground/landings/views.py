edbda95b6092b7f87e1d99bfb372066c7c742b14
jhosuapp <jhosuapvll@gmail.com>
2025-12-12 11:00:04 -0500

    feat: DTL

--- 04-playground/landings/views.py
+++ 04-playground/landings/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, "landings/landing.html")
+    response = { "name": "Jhosua" }
+    return render(request, "landings/landing.html", response)
