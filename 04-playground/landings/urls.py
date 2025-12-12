853106c73857d450a78d3617de07a9baf6a859af
jhosuapp <jhosuapvll@gmail.com>
2025-12-12 16:19:04 -0500

    feat: tags and filters

--- 04-playground/landings/urls.py
+++ 04-playground/landings/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home, name='home')
+    path('home', views.home, name='home'),
+    path('stack/<str:tool>', views.stack, name='stack')
 ]
--- 04-playground/landings/views.py
+++ 04-playground/landings/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from datetime import date
 
+# Create your views here.
 def home(request):
-    response = { "name": "Jhosua" }
+    today = date.today()
+    stack = [
+        {
+            "id": "python", 
+            "name": "Python"
+        },
+        {
+            "id": "node", 
+            "name": "Node"
+        },
+        {
+            "id": "javascript", 
+            "name": "Javascript"
+        },
+    ]
+    response = { 
+                "today": today, 
+                "stack": stack,
+                "name": "Jhosua", 
+                "age": 24,
+            }
     return render(request, "landings/landing.html", response)
+
+def stack(request, tool):
+    return HttpResponse(f'Tu tecnología es: { tool }')
