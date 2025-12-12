1d72daf5cfd07a14999821f6133383f5ee38f717
jhosuapp <jhosuapvll@gmail.com>
2025-12-12 09:03:21 -0500

    feat: render HTML

--- 04-playground/quotes/urls.py
+++ 04-playground/quotes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.index),
     path("days-by-number/<int:day>", views.days_week_with_number),
     path("days/<str:day>", views.days_week, name='day-quote')
 ]
--- 04-playground/quotes/views.py
+++ 04-playground/quotes/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, HttpResponseBadRequest
 from django.shortcuts import redirect
+from django.urls import reverse
 
 
 # Create your views here.
@@ -15,18 +15,31 @@ days_of_week = {
     "sunday": "sunday"
 }
 
+def index(request):
+    list_items = ''
+    days = list(days_of_week.keys())
+    
+    for day in days:
+        day_path = reverse('day-quote',  kwargs={'day': day}) 
+        list_items += f'<li><a href="{day_path}">{ day }</a></li>'
+    
+    ul = f'<ul>{list_items}</ul>'
+    
+    return HttpResponse(ul)
+
 def days_week(request, day):
     try:
         return HttpResponse(days_of_week[day])
-    except: 
-        return HttpResponseRedirect('ese día no existe')    
+    except Exception: 
+        return HttpResponseNotFound('ese día no existe')    
     
 
 def days_week_with_number(request, day):
     try:
         days = list(days_of_week.keys())
         if day-1 > len(days):
-            # return HttpResponseRedirect('day-quote')      
+            # return HttpResponseRedirect('day-quote')
+            # redirect_path = reverse('day-quote',  kwargs={'day': 'monday'})      
             return redirect('day-quote', day='monday')
         else:
             return HttpResponse(days_of_week[days[day-1]])        
