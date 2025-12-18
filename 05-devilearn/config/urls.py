8098a986370fbb53e9d953897966079d6543936a
jhosuapp <jhosuapvll@gmail.com>
2025-12-17 20:27:31 -0500

    feat: dashboard ui

--- 05-devilearn/config/urls.py
+++ 05-devilearn/config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('courses/', include('apps.courses.urls'))
+    path('courses/', include('apps.courses.urls')),
+    path('dashboard/', include('apps.dashboard.urls'))
 ]
