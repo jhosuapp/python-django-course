57b72f7be1babe22cc0b46ca774e71e75e5ec1c8
jhosuapp <jhosuapvll@gmail.com>
2025-12-29 14:57:12 -0500

    feat: save and create by ORM

--- 04-playground/minilibrary/models.py
+++ 04-playground/minilibrary/models.py
@@ -6,9 +6,15 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     birth_date = models.DateField(null=True, blank=True)
     
+    def __str__(self):
+        return self.name
+    
 class Book(models.Model):
     title = models.CharField(max_length=200)
     publication_date = models.DateField(null=True, blank=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
     pages = models.IntegerField()
     isbn = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.title
