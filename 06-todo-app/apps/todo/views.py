d17727aaafe4ac4d9eb3e804f109594a3a29bb37
jhosuapp <jhosuapvll@gmail.com>
2025-12-31 10:52:57 -0500

    feat: ui TODOS

--- 06-todo-app/apps/todo/views.py
+++ 06-todo-app/apps/todo/views.py
@@ -3,5 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 
 def indexTodo(request):
-    response = { 'test': 'test' }
+    todos = [
+        {
+            "title": "Título de mi primera TODO",
+            "description": "descripción de mi TODO",
+            "is_checked": False,
+        },
+        {
+            "title": "Título de mi segunda TODO",
+            "description": "descripción de mi TODO",
+            "is_checked": True,
+        },
+    ]
+    response = { 'todos': todos }
     return render(request, 'todo/index.html', response)
