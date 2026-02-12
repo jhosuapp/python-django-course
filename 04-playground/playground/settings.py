f95e7884a4c7f557a75fd648da293f097a6d3d97
jhosuapp <jhosuapvll@gmail.com>
2026-02-12 15:29:16 -0500

    feat: singers practice

--- 04-playground/playground/settings.py
+++ 04-playground/playground/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'landings',
     'quotes',
     'minilibrary',
+    'singers',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- 04-playground/singers/models.py
+++ 04-playground/singers/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Singer(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Song(models.Model):
+    song_name = models.CharField(max_length=200)
+    publication_date = models.DateField(null=True, blank=True)
+    singer = models.ForeignKey(Singer, on_delete=models.PROTECT, related_name='singers')
+    duration = models.IntegerField()
+    
+    def __str__(self):
+        return self.song_name
