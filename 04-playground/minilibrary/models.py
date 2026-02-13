9ba59cb63e077b5a00c3a795dc2a937f06aabf97
jhosuapp <jhosuapvll@gmail.com>
2026-02-13 14:12:57 -0500

    feat: ManyToMany, OneToOne OneToMany model relations

--- 04-playground/minilibrary/models.py
+++ 04-playground/minilibrary/models.py
@@ -9,12 +9,25 @@ class Author(models.Model):
     def __str__(self):
         return self.name
     
+class Genre(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.name
+    
 class Book(models.Model):
     title = models.CharField(max_length=200)
     publication_date = models.DateField(null=True, blank=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
     pages = models.IntegerField()
     isbn = models.CharField(max_length=50)
+    genres = models.ManyToManyField(Genre, related_name='books')
     
     def __str__(self):
         return self.title
+    
+class BookDetail(models.Model):
+    summary = models.TextField()
+    cover_url = models.CharField()
+    language = models.CharField()
+    book = models.OneToOneField(Book, on_delete=models.CASCADE, related_name='detail')
