69965c6d6ef75abd62cc5c3360b98173a63e87b7
jhosuapp <jhosuapvll@gmail.com>
2026-03-02 15:32:12 -0500

    feat: custom detail

--- 04-playground/minilibrary/admin.py
+++ 04-playground/minilibrary/admin.py
@@ -2,10 +2,17 @@ from django.contrib import admin
 from .models import Author, Genre, Book, BookDetail, Review, Loan
 
 # Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'pages', 'publication_date')
+    search_fields = ('title', 'author__name')
+    list_filter = ('author', 'genres', 'publication_date')
+    ordering = ['publication_date']
+    date_hierarchy = 'publication_date'
 
 admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(Book)
+# admin.site.register(Book, BookAdmin)
 admin.site.register(BookDetail)
 admin.site.register(Review)
 admin.site.register(Loan)
